39fa7fd4cbd4d0770a88b29c151f5338769305bf
abrabant <abrabant@student.42.fr>
2021-09-10 18:37:13 +0200

    Definition of Character class, implementing the ICharacter interface

--- module-04/ex03/Character.cpp
+++ module-04/ex03/Character.cpp
@@ -1,5 +1,42 @@
+#include <iostream>
 #include "Character.hpp"
 
-Character::Character(void)
+using std::cerr;
+
+Character::Character(std::string const & name): _name(name), _materia()
 {
+}
+
+void Character::equip(AMateria *m)
+{
+	for (size_t i = 0; i != _inventory_size; ++i) {
+		// if slot is empty, materia is equiped there.
+		if (!_materia[i]) {
+			_materia[i] = m;
+			return ;
+		}
+	}
+
+	cerr << "Could not equip materia: inventory is full\n";
+}
+
+void Character::unequip(int idx)
+{
+	if (!_materia[idx]) {
+		cerr << "Nothing in slot #" << idx << " of player's inventory\n";
+	}
+}
+
+void Character::use(int idx, ICharacter & target)
+{
+	if (!_materia[idx]) {
+		cerr << "Nothing in slot #" << idx << " of player's inventory\n";
+	}
+	// TODO: use materia when AMateria::use is properly implemented
+	(void) target;
+}
+
+std::string const & Character::getName(void) const
+{
+	return _name;
 }
--- module-04/ex03/Character.hpp
+++ module-04/ex03/Character.hpp
@@ -1,9 +1,21 @@
 #ifndef CHARACTER_HPP
 # define CHARACTER_HPP
 # include "ICharacter.hpp"
+# include "AMateria.hpp"
 
 class Character: public ICharacter
 {
+	static const size_t _inventory_size = 4;
+	
+	std::string _name;
+	AMateria* _materia[_inventory_size];
+
+	public:
+		Character(std::string const & name = "default");
+		std::string const & getName() const;
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
 };
 
 #endif
